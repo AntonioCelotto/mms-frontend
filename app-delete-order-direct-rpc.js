89e677a0797373e224f454627d8218eeb71a9560
AntonioCelotto <a.celotto@newdigitalapp.com>
2026-07-17 20:06:50 +0200

    Fix direct delete RPC anon key

--- app-delete-order-direct-rpc.js
+++ app-delete-order-direct-rpc.js
@@ -1,7 +1,7 @@
 (function () {
   const SUPABASE_URL = "https://fzdqemzowxjuotqalaol.supabase.co";
   const SUPABASE_ANON_KEY =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJIUzI1NiIsInJlZiI6ImZ6ZHFlbXpvd3hqdW90cWFsYW9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5Njg3NzYsImV4cCI6MjA5NTU0NDc3Nn0.fmZ9RThFxnaJGQsOYeu_ZjjUNHThlRX87qz9sX4N6Mk";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6ZHFlbXpvd3hqdW90cWFsYW9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5Njg3NzYsImV4cCI6MjA5NTU0NDc3Nn0.fmZ9RThFxnaJGQsOYeu_ZjjUNHThlRX87qz9sX4N6Mk";
 
   function isDeleteRecordUrl(input) {
     const url = typeof input === "string" ? input : input?.url || "";
